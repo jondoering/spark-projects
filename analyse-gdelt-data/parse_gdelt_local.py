aba4c77a943e4083c20dbab36a87404ab8ff63d5
jondoering <mail@jonathan-doering.com|>
2018-02-12 14:00:25 +0000

    added gdelt project

---
+++ analyse-gdelt-data/parse_gdelt_local.py
@@ -0,0 +1,15 @@
+#parses
+from gdelt_data_parser import gdelt_data_parser as gdp
+
+from pyspark import SparkConf, SparkContext
+conf = SparkConf().setMaster("local").setAppName("MinTemperatures")
+sc = SparkContext(conf = conf)
+
+
+#read all export.CSV data into a single RDD
+lines = sc.textFile("./data/*.gkg.csv")
+export_data = lines.map(gdp.parse_gkg_data)
+
+
+for l in export_data.collect()[0:2]:
+    print(l)
